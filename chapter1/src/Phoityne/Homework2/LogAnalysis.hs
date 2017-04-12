06683b2fc5be7714512337263ba36ed7cdd20b18
Pedro Pereira <pedropereira@Pedros-Air.lan>
2017-04-12 01:17:59 +0100

    ParseMessage and Parse

--- chapter1/src/Phoityne/Homework2/LogAnalysis.hs
+++ chapter1/src/Phoityne/Homework2/LogAnalysis.hs
@@ -2,3 +2,18 @@
 module LogAnalysis where
 
 import Log
+
+createLogMessage :: MessageType -> String -> [String] -> LogMessage
+createLogMessage mt t l = LogMessage mt (read t) (unwords l)
+
+parseMessage :: String -> LogMessage
+parseMessage s = case words s of 
+    ("E":errType:timeStamp:message) -> createLogMessage (Error (read errType)) timeStamp message
+    ("W":timeStamp:message)         -> createLogMessage Warning timeStamp message
+    ("I":timeStamp:message)         -> createLogMessage Info timeStamp message
+    list -> Unknown (unwords list)
+
+parse :: String -> [LogMessage]
+parse messages = case lines messages of
+    (s:xs) -> (parseMessage s) : (parse (unlines xs))
+    [] -> []
--- chapter1/test/SpecHelper.hs
+++ chapter1/test/SpecHelper.hs
@@ -2,8 +2,10 @@ module SpecHelper
     ( module Test.Hspec
     , module Phoityne.Example.Math
     , module Phoityne.Example.CreditCard
+    , module LogAnalysis
     ) where
 
 import Test.Hspec
 import Phoityne.Example.Math
 import Phoityne.Example.CreditCard
+import LogAnalysis
